a5122d9f0fd9b5bff501ef72f0d75a4c89433919
JayA96 <jay-akred96@hotmail.com>
2020-06-29 19:20:03 +0100

    debugging api endpoints

--- application.py
+++ application.py
@@ -34,18 +34,17 @@ def callback():
     tracks = get_saved_tracks(api_url, auth_header)
 
     # Get genre and release decade for each track
-    #for i in range(len(tracks)):
-    artist_id = tracks[0]["artist_id"]
-    artist_api_endpoint = f"{api_url}/artists/{artist_id}"
-    artist_response = requests.get(artist_api_endpoint, headers=auth_header)
-    artist_data = json.loads(artist_response.text)
-    genres = artist_data["genres"]
-    return render_template("test.html", tracks=genres)
-    """tracks[i]["decade"] = get_decade(tracks[i])
-    tracks[i]["genre"] = get_main_genre(genres)
+    for i in range(len(tracks)):
+        artist_id = tracks[i]["artist_id"]
+        artist_api_endpoint = f"{api_url}/artists/{artist_id}"
+        artist_response = requests.get(artist_api_endpoint, headers=auth_header)
+        artist_data = json.loads(artist_response.text)
+        genres = artist_data["genres"]
+        tracks[i]["decade"] = get_decade(tracks[i])
+        tracks[i]["genre"] = get_main_genre(genres)
 
-    # Get audio features for all tracks
-    get_audio_features(tracks, api_url, auth_header)
+        # Get audio features for all tracks
+        get_audio_features(tracks, api_url, auth_header)
     return redirect("/home")
 
 
@@ -80,7 +79,7 @@ def custom():
     else:
         playlist_id = sort_by_audio_features(tracks, custom_data, api_url, auth_header)
     return render_template("playlists.html", playlist_type="custom", id=playlist_id)
-"""
+
 
 if __name__ == "__main__":
     app.run(port=port)
